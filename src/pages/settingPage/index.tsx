832c483aaaaf1de302bb1234ad454f1a96d66899
Myugaa <poodlesama@naver.com>
2023-04-23 14:53:38 +0900

    git branch -a foldername

--- src/pages/settingPage/index.tsx
+++ src/pages/settingPage/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import * as S from './style'
+
 
 function SettingPage() {
-  return <div>SettingPage</div>
+  return <div>
+    <S.TopBar>설정</S.TopBar>
+    <S.SettingWrapper>
+      <S.SettingMenus>
+        <S.SettingModal></S.SettingModal>
+      </S.SettingMenus>
+    </S.SettingWrapper>
+    <S.Nav></S.Nav>
+  </div>
 }
 
 export default SettingPage
---
+++ src/pages/settingPage/style.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const TopBar = styled.div`
+  display: flex;
+  position: absolute;
+  
+  top: 30px;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  border-bottom: 2px solid ${({ theme }) => theme.grey01};
+`
+
+export const SettingWrapper = styled.div`
+  display: flex;
+  position: absolute;
+  
+  top: 30px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: 
+`
+export const Nav = styled.div`
+  display: flex;
+  position: absolute;
+  
+  bottom: 30px;
+  left: 0;
+  width: 100%;
+  height: 60px;
+`
+`
